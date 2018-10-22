c8b5da9b309989e683304ad491f7a2b6e530a39b
bpahuja <bpahuja_be16@thapar.edu>
2018-10-22 10:26:05 +0530

    Login Begin

--- homepage/urls.py
+++ homepage/urls.py
@@ -6,10 +6,14 @@ from django.conf.urls import url,include
 from django.conf.urls.static import static
 from django.conf import settings
 from . import views
+from django.contrib.auth import views as auth_views
 from django.conf.urls import url
 from . import views
 
 
 urlpatterns = [
 	url(r'^$', views.home, name='home'),
+	url(r'^login/$', auth_views.login,{'template_name': 'homepage/login.html'}),
+	url(r'^logout/$', auth_views.logout,{'template_name': 'homepage/logged_out.html'}),
+	url(r'^register/', views.register),
 	]
--- homepage/views.py
+++ homepage/views.py
@@ -1,50 +1,28 @@
 from django.shortcuts import render
-from django.http import Http404
-
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponseRedirect
+from django import forms
+from .forms import UserRegistrationForm
 # Create your views here.
-from .models import *
-
-
 def home(request):
-    
     return render(request, 'homepage/home.html')
-
-
-def login_view(request):
-    print(request.user.is_authenticated())
-    title = "Login"
-    form = UserLoginForm(request.POST or None)
-    if form.is_valid():
-        username  = form.cleaned_data.get("username")
-        password = form.cleaned_data.get('password')
-        user = authenticate(username=username, password=password)
-        login(request,user)
-        print(request.user.is_authenticated())
-        #redirect
-        return redirect("/home")
-    return render(request, "home/form.html", {"form":form,"title":title})
-
-def logout_view(request):
-    logout(request)
-    return redirect("/home")
-
-User = get_user_model()
-def register_view(request):
-    print(request.user.is_authenticated())
-    title = "Register"
-    form = UserRegisterForm(request.POST or None)
-    if form.is_valid():
-        user=form.save(commit=False)
-        password=form.cleaned_data.get('password')
-        user.set_password(password)
-        user.save()
-
-        new_user = authenticate(username=user.username, password=password)
-        login(request, new_user)
-        return redirect("/home")
-
-    context = {
-        "form": form,
-        "title": title
-    }
-    return render(request, "home/form.html", context)
+    
+def register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            userObj = form.cleaned_data
+            username = userObj['username']
+            email =  userObj['email']
+            password =  userObj['password']
+            if not (User.objects.filter(username=username).exists() or User.objects.filter(email=email).exists()):
+                User.objects.create_user(username, email, password)
+                user = authenticate(username = username, password = password)
+                login(request, user)
+                return HttpResponseRedirect('/')
+            else:
+                raise forms.ValidationError('Looks like a username with that email or password already exists')
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'homepage/register.html', {'form' : form})
