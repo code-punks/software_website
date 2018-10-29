ddaf27933bcd8265e63413d903eac0189a549f4f
bpahuja <bpahuja_be16@thapar.edu>
2018-10-29 10:43:22 +0530

    Confirm Password added

--- homepage/forms.py
+++ homepage/forms.py
@@ -17,3 +17,15 @@ class UserRegistrationForm(forms.Form):
         max_length = 32,
         widget = forms.PasswordInput()
     )
+
+    confirm_password=forms.CharField(widget=forms.PasswordInput())
+
+    def clean(self):
+        cleaned_data = super(UserRegistrationForm, self).clean()
+        password = cleaned_data.get("password")
+        confirm_password = cleaned_data.get("confirm_password")
+
+        if password != confirm_password:
+            raise forms.ValidationError(
+                "password did not match"
+            )
